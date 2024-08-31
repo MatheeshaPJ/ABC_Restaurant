09261ba5e33c13fc8e2c45b6690f923cd20a77a3
Matheesha <matheeshajayasinghe76@gmail.com>
2024-08-31 19:07:48 +0530

    Create Analytics.jsx

---
+++ frontend/src/components/Analytics.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import burger from '../assets/burger.png'
+
+const Analytics = () => {
+    return (
+        <div className='w-full bg-white py-16 px-4'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+                <img className='w-[500px] mx-auto my-4' src={burger} alt="burger.png" />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#00df9a] font-bold'> HUNGRY BURGER</p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Just Order When You Feel Hungry!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam illo excepturi modi odio maxime repellat illum neque commodi totam ullam rem debitis esse accusantium, quaerat omnis eaque ea officiis vitae!</p>
+                    <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Order Now</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
