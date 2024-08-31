3d97df9e58c517402b37000c150021a378092645
Matheesha <matheeshajayasinghe76@gmail.com>
2024-08-31 23:38:15 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,11 +21,11 @@ const Navbar = () => {
             </ul>
 
             <div onClick={handleNav} className='block md:hidden'>
-                {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+                {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
                
             </div>
 
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out' : 'fixed left-[-100%]'}>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
 
                 <ul className='p-4 uppercase'>
