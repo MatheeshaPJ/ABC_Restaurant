c2dbd0669fc79fcd93aca7e084517984d8d28ab4
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-01 19:08:51 +0530

    Update Newsletter.jsx

--- frontend/src/components/Newsletter.jsx
+++ frontend/src/components/Newsletter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Newsletter = () => {
     return (
-        <div className='w-full py-16 text-white px-4'>
+        <div className='w-full py-16 text-white px-4 bg-black'>
             <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
                 <div className='lg:col-span-2 my-2'>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want to more about our services?</h1>
@@ -13,7 +13,7 @@ const Newsletter = () => {
                         <input className='p-3 flrex w-full rounded-md text-black' type="email" placeholder='Enter Email' />
                         <button className='bg-[#00df9a] rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3 text-black'>Send</button>
                     </div>
-                    <p>We care about your satisfaction. Reach <span className='text-[#00df9a]'>Our Services</span></p>
+                    <p>We care about your satisfaction. Reach <span className='text-[#00df9a] cursor-pointer'>Our Services</span></p>
                 </div>
             </div>
         </div>
