ab42e5f2395d6ed6bd6a62c96abb82f880fb7ba3
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-02 13:07:09 +0530

    Update Menu.jsx

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div>
+            <Navbar/>
+            <Footer/>
+    </div>
   )
 }
 
