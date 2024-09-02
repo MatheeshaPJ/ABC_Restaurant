f3a6b59563d8d4bae425786a60912618a4506fd6
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-02 13:07:07 +0530

    Update Order.jsx

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Order = () => {
   return (
-    <div>Order</div>
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
   )
 }
 
