4f2c2b408bb0cba3f1f65e568f65ca46c48e51c9
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-02 13:07:25 +0530

    Update Analytics.jsx

--- frontend/src/components/Analytics.jsx
+++ frontend/src/components/Analytics.jsx
@@ -11,7 +11,7 @@ const Analytics = () => {
                     <p className='text-[rgb(148,196,66)] font-bold'> HUNGRY BURGER💦</p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Just Order When You Feel Hungry!</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam illo excepturi modi odio maxime repellat illum neque commodi totam ullam rem debitis esse accusantium, quaerat omnis eaque ea officiis vitae!</p>
-                    <button className='bg-black text-[#d9b534] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 hover:scale-105 duration-300'>Order Now</button>
+                    <button className='bg-black text-[#d19831] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 hover:scale-105 duration-300'>Order Now</button>
                 </div>
                 <img className='w-[500px] mx-auto my-4 hover:scale-105 duration-300' src={burger} alt="burger.png" />
 
