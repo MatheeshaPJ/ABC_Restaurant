5fa48cee200b5adc7c7836446d42a7ab7c72a6ba
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-04 16:49:46 +0530

    Create UserService.java

---
+++ backend/src/main/java/com/abc/backend/service/UserService.java
@@ -0,0 +1,25 @@
+package com.abc.backend.service;
+
+import com.abc.backend.model.User;
+import com.abc.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
