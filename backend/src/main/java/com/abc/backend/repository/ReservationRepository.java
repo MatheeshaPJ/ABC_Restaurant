367596c85aaf615c6a2a9743b93392c699bd68ab
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-04 16:53:14 +0530

    Create ReservationRepository.java

---
+++ backend/src/main/java/com/abc/backend/repository/ReservationRepository.java
@@ -0,0 +1,15 @@
+package com.abc.backend.repository;
+
+import com.abc.backend.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByUserId(Long userId);
+    List<Reservation> findByUsername(Long username);
+    List<Reservation> findByEmail(Long email);
+
+}
