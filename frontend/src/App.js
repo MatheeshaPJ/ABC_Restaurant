58305aa098de59849af515e12244963ac7c244ab
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-04 13:09:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,15 @@ import ManageOrder from "./admin/ManageOrder";
 import ManageUsers from "./admin/ManageUsers";
 import ManageContent from "./admin/ManageContent";
 import ReportGeneration from "./admin/ReportGeneration";
-import SystemSettings from "./admin/SystemSettings";
+import SystemSettings from "./admin/SystemSettings";  
+import StaffLogin from "./staff/StaffLogin";  
+import StaffDashboard from "./staff/StaffDashboard";  
+import StaffManageReservations from "./staff/ManageReservations";  
+import StaffManageQueries from "./staff/ManageQueries";  
+import StaffManageOrders from "./staff/ManageOrders";  
+import StaffUpdateProfile from "./staff/UpdateProfile";  
+import StaffSystemSettings from "./staff/SystemSettings";  
+import StaffLogout from "./staff/Logout";  
 
 function App() {
   return (
@@ -26,6 +34,8 @@ function App() {
       <Router>
 
       <Routes>
+
+        {/* Routing Customer pages' paths */}
         <Route exact path="/" element={ <Home/>}></Route>
         <Route exact path="/signin" element={ <Signin/>}></Route>
         <Route exact path="/signup" element={ <Signup/>}></Route>
@@ -36,6 +46,7 @@ function App() {
         <Route exact path="/about" element={ <About/>}></Route>
         <Route exact path="/contact" element={ <Contact/>}></Route>
 
+        {/* Routing Admin pages' paths */}
         <Route exact path="/admin" element={ <AdminLogin/>}></Route>
         <Route exact path="/admin/dashboard" element={ <AdminDashboard/>}></Route>
         <Route exact path="/admin/reservations" element={ <ManageReservations/>}></Route>
@@ -45,6 +56,17 @@ function App() {
         <Route exact path="/admin/content" element={ <ManageContent/>}></Route>
         <Route exact path="/admin/reports" element={ <ReportGeneration/>}></Route>
         <Route exact path="/admin/settings" element={ <SystemSettings/>}></Route>
+
+        {/* Routing Staff pages' paths */}
+        <Route exact path="/staff" element={ <StaffLogin/>}></Route>
+        <Route exact path="/staff/dashboard" element={ <StaffDashboard/>}></Route>
+        <Route exact path="/staff/reservations" element={ <StaffManageReservations/>}></Route>
+        <Route exact path="/staff/queries" element={ <StaffManageQueries/>}></Route>
+        <Route exact path="/staff/orders" element={ <StaffManageOrders/>}></Route>
+        <Route exact path="/staff/profile" element={ <StaffUpdateProfile/>}></Route>
+        <Route exact path="/staff/settings" element={ <StaffSystemSettings/>}></Route>
+        <Route exact path="/staff/logout" element={ <StaffLogout/>}></Route>
+
        
 
      
