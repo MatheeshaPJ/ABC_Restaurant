e4c7317dc9b448fd9a2d4de94b970499abacd884
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-04 17:56:17 +0530

    Create UserNotFoundException.java

---
+++ backend/src/main/java/com/abc/backend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.abc.backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
