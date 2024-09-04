3487454c894ce6774f8a163ece71fb32713fe0e9
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-04 16:49:52 +0530

    Create UserController.java

---
+++ backend/src/main/java/com/abc/backend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.abc.backend.controller;
+
+//public class UserController {
+//
+//}
+import com.abc.backend.model.User;
+import com.abc.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+//    @PostMapping("/login")
+//    public String loginUser(@RequestBody LoginRequest loginRequest) {
+//        User user = userService.findByUsername(loginRequest.getUsername());
+//        if (user != null && user.getPassword().equals(loginRequest.getPassword())) {
+//            // Generate and return JWT token
+//            return "JWT_TOKEN";
+//        }
+//        throw new RuntimeException("Invalid credentials");
+//    }
+}
