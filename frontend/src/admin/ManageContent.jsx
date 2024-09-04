ff7dd0d0959ef0ad659d0251e55d7aa4100f296b
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-04 13:09:09 +0530

    Update ManageContent.jsx

--- frontend/src/admin/ManageContent.jsx
+++ frontend/src/admin/ManageContent.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import AdminNavbar from './AdminNavbar';
 
 
 const ManageContent = () => {
@@ -42,9 +43,9 @@ const ManageContent = () => {
 
   return (
     <div className='bg-black text-white min-h-screen flex flex-col'>
+<AdminNavbar/>
 
-
-      <div className='flex-grow'>
+      <div className='flex-grow mt-16'>
         <div className='max-w-[1240px] mx-auto py-12'>
           <h1 className='text-4xl font-bold text-center mb-8 text-[#d4af37]'>Content Management</h1>
 
