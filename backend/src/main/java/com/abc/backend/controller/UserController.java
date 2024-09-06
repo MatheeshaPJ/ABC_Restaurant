d29fb1abbafcabad65939120eeac98bd4d4a89fa
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-06 13:06:34 +0530

    Update UserController.java

--- backend/src/main/java/com/abc/backend/controller/UserController.java
+++ backend/src/main/java/com/abc/backend/controller/UserController.java
@@ -3,11 +3,12 @@ package com.abc.backend.controller;
 import com.abc.backend.model.User;
 import com.abc.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -16,6 +17,11 @@ public class UserController {
     @PostMapping("/user/register")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("user/getallusers")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
 }
