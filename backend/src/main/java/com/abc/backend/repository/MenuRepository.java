8c3a68663dad6b7f82beb2bd77af53d99544765e
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-09 18:07:02 +0530

    Update MenuRepository.java

--- backend/src/main/java/com/abc/backend/repository/MenuRepository.java
+++ backend/src/main/java/com/abc/backend/repository/MenuRepository.java
@@ -2,6 +2,8 @@ package com.abc.backend.repository;
 
 import com.abc.backend.model.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
