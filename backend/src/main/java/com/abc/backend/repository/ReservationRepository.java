af9cbb569e168baddff36862e9a718c959319497
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-10 18:07:21 +0530

    Update ReservationRepository.java

--- backend/src/main/java/com/abc/backend/repository/ReservationRepository.java
+++ backend/src/main/java/com/abc/backend/repository/ReservationRepository.java
@@ -1,7 +1,20 @@
 package com.abc.backend.repository;
 
+
 import com.abc.backend.model.Reservation;
+import com.abc.backend.model.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+
+    // Query to find reservations for a given date, time, and location
+    @Query("SELECT r.table FROM Reservation r WHERE r.reservationDate = :date AND r.reservationTime = :time AND r.location = :location")
+    List<RestaurantTable> findReservedTables(@Param("date") LocalDate date, @Param("time") LocalTime time, @Param("location") String location);
 }
+
