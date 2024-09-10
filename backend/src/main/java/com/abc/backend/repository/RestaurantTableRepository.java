d3c0cb0c8a43f23a9c9d957d781db946b8913ce8
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-10 18:07:26 +0530

    Update RestaurantTableRepository.java

--- backend/src/main/java/com/abc/backend/repository/RestaurantTableRepository.java
+++ backend/src/main/java/com/abc/backend/repository/RestaurantTableRepository.java
@@ -3,5 +3,15 @@ package com.abc.backend.repository;
 import com.abc.backend.model.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
+    // Find all tables except those with the specified tableIds
+    List<RestaurantTable> findByTableIdNotIn(List<Long> reservedTableIds);
+
+    // Query to find tables for a given location
+    List<RestaurantTable> findByLocation(String location);
+
 }
