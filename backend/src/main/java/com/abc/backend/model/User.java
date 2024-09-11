fff917fae16205c93bd295f37b988ec1f9358e0c
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-11 05:54:42 +0530

    Update User.java

--- backend/src/main/java/com/abc/backend/model/User.java
+++ backend/src/main/java/com/abc/backend/model/User.java
@@ -1,15 +1,18 @@
 package com.abc.backend.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
+@Setter
+@Getter
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -29,62 +32,5 @@ public class User {
     @Column(nullable = false)
     private String address;
 
-    //Getters & Setters
 
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public USER_ROLE getRole() {
-        return role;
-    }
-
-    public void setRole(USER_ROLE role) {
-        this.role = role;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
