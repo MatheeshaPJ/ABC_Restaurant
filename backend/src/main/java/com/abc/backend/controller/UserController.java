7204840927ece31061daa7c62857a984a1449ef8
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-11 05:55:49 +0530

    Update UserController.java

--- backend/src/main/java/com/abc/backend/controller/UserController.java
+++ backend/src/main/java/com/abc/backend/controller/UserController.java
@@ -2,74 +2,61 @@ package com.abc.backend.controller;
 
 import com.abc.backend.exception.UserNotFoundException;
 import com.abc.backend.model.User;
-import com.abc.backend.repository.UserRepository;
+import com.abc.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
+    // Register a new user
     @PostMapping("/user/register")
-    User newUser(@RequestBody User newUser){
-        return userRepository.save(newUser);
+    public ResponseEntity<User> registerUser(@RequestBody User newUser) {
+        User user = userService.registerUser(newUser);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    // Get all users
     @GetMapping("/user/getallusers")
-    List<User> getAllUsers(){
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    // Get user by ID
     @GetMapping("/user/{id}")
-    User getUserById(@PathVariable Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(id));
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    // Update user by ID
     @PutMapping("/user/{id}")
-    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
-        return userRepository.findById(id)
-                .map(user -> {
-                    user.setEmail(newUser.getEmail());
-                    user.setFirstName(newUser.getFirstName());
-                    user.setLastName(newUser.getLastName());
-                    user.setPassword(newUser.getPassword());
-                    user.setAddress(newUser.getAddress());
-                    user.setRole(newUser.getRole());
-                    return userRepository.save(user);
-                }).orElseThrow(() -> new UserNotFoundException(id));
+    public ResponseEntity<User> updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        User updatedUser = userService.updateUser(newUser, id);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
+    // Delete user by ID
     @DeleteMapping("/user/{id}")
-    String deleteUser(@PathVariable Long id){
-        if(!userRepository.existsById(id)){
-            throw new UserNotFoundException(id);
-        }
-        userRepository.deleteById(id);
-        return  "User with id "+id+" has been deleted success.";
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>("User with ID " + id + " has been deleted successfully.", HttpStatus.OK);
     }
 
-    // Add Login functionality here
-    @PostMapping("/login")
-    public User loginUser(@RequestBody User loginUser) {
-        Optional<User> user = userRepository.findByEmail(loginUser.getEmail());
-
-        if (user.isPresent() && user.get().getPassword().equals(loginUser.getPassword())) {
-            // If email and password match, return the user
-            return user.get();
-        } else {
-            // Throw an exception if the credentials are incorrect
-            throw new UserNotFoundException("Invalid email or password.");
-        }
+    // Login user
+    @PostMapping("/user/login")
+    public ResponseEntity<User> loginUser(@RequestBody User loginUser) {
+        User user = userService.loginUser(loginUser.getEmail(), loginUser.getPassword());
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
 }
-
-
