b0f29144be6899b0281ffa709ca1ff0930541ee8
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-11 05:55:58 +0530

    Update Category.java

--- backend/src/main/java/com/abc/backend/model/Category.java
+++ backend/src/main/java/com/abc/backend/model/Category.java
@@ -1,7 +1,11 @@
 package com.abc.backend.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Category {
 
@@ -12,22 +16,5 @@ public class Category {
     @Column(nullable = false)
     private String categoryName;
 
-    // Getters and Setters
 
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
