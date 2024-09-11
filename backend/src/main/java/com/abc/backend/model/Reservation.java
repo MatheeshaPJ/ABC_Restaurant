38db7699b4b4ecec116bff2bb124a73aa6a86f08
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-11 05:55:01 +0530

    Update Reservation.java

--- backend/src/main/java/com/abc/backend/model/Reservation.java
+++ backend/src/main/java/com/abc/backend/model/Reservation.java
@@ -8,6 +8,8 @@ import java.time.LocalTime;
 import java.time.LocalDateTime;
 
 
+@Setter
+@Getter
 @Entity
 public class Reservation {
 
@@ -49,94 +51,4 @@ public class Reservation {
     @Column(columnDefinition = "TIMESTAMP", nullable = false, updatable = false)
     private LocalDateTime timestamp = LocalDateTime.now(); // Auto-generated timestamp
 
-    //Getters & Setters
-
-
-    public Long getReservationId() {
-        return reservationId;
-    }
-
-    public void setReservationId(Long reservationId) {
-        this.reservationId = reservationId;
-    }
-
-    public LocalDate getReservationDate() {
-        return reservationDate;
-    }
-
-    public void setReservationDate(LocalDate reservationDate) {
-        this.reservationDate = reservationDate;
-    }
-
-    public LocalTime getReservationTime() {
-        return reservationTime;
-    }
-
-    public void setReservationTime(LocalTime reservationTime) {
-        this.reservationTime = reservationTime;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getSpecialNote() {
-        return specialNote;
-    }
-
-    public void setSpecialNote(String specialNote) {
-        this.specialNote = specialNote;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public RestaurantTable getTable() {
-        return table;
-    }
-
-    public void setTable(RestaurantTable table) {
-        this.table = table;
-    }
-
-    public User getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(User customer) {
-        this.customer = customer;
-    }
-
-    public String getReserveUnder() {
-        return reserveUnder;
-    }
-
-    public void setReserveUnder(String reserveUnder) {
-        this.reserveUnder = reserveUnder;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
 }
