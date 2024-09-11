f9a9851c2ea67d6e0bca4103689355532b809176
Matheesha <matheeshajayasinghe76@gmail.com>
2024-09-11 05:56:13 +0530

    Delete OrderItemRepository.java

--- backend/src/main/java/com/abc/backend/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abc.backend.repository;
-
-
-import com.abc.backend.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
